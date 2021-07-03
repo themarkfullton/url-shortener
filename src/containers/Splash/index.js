c954894e5ec979fd9433bcaf75259dcc0881e710
Mark <the.mark.fullton@gmail.com>
2021-07-02 20:08:10 -0700

    Added style to key-value display

--- src/containers/Splash/index.js
+++ src/containers/Splash/index.js
@@ -1,5 +1,6 @@
 import React, {useState} from 'react';
 import ExampleForm from '../ExampleForm';
+import splashStyle from "./splash.module.css";
 
 const Splash = () => {
     const [currValues, setCurrValues] = useState({ fName: "", lName: "", somethingElse: ""});
@@ -12,9 +13,11 @@ const Splash = () => {
     return formSubmitted ? (
         <div>
             <h1>Values Submitted</h1>
+            <div className={splashStyle.container}>
             <h3>{Object.entries(currValues).map((x, idx) => {
-                return <p key={idx}>{x[0]}: {x[1]}</p>
+                return <p key={idx}><span className={splashStyle.bold}>{x[0]}:</span> {x[1]}</p>
             })}</h3>
+            </div>
         </div>
     ) : (
         <div>
